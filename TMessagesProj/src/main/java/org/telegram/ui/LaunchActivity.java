9f47d0303cb63eab7cff79c98401515b932a9550
5generation223-lab <5generation.223@gmail.com>
2026-09-22 19:06:55 +0300

    Update LaunchActivity.java

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -442,6 +442,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         flagSecureReason.attach();
 
         super.onCreate(savedInstanceState);
+        TeliAutoEmojis.installAll(currentAccount);
         if (Build.VERSION.SDK_INT >= 24) {
             AndroidUtilities.isInMultiwindow = isInMultiWindowMode();
         }
