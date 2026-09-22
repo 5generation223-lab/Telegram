fa8f0800adf0bf4b51832316671a429de73ebec7
5generation223-lab <5generation.223@gmail.com>
2026-09-22 18:05:10 +0300

    Update BuildVars.java

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -21,12 +21,12 @@ public class BuildVars {
     public static boolean DEBUG_VERSION = BuildConfig.DEBUG_VERSION;
     public static boolean LOGS_ENABLED = BuildConfig.DEBUG_VERSION;
     public static boolean DEBUG_PRIVATE_VERSION = BuildConfig.DEBUG_PRIVATE_VERSION;
-    public static boolean USE_CLOUD_STRINGS = true;
-    public static boolean CHECK_UPDATES = true;
+    public static boolean USE_CLOUD_STRINGS = false;
+    public static boolean CHECK_UPDATES = false;
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static String BUILD_VERSION_STRING = BuildConfig.BUILD_VERSION_STRING;
 
-    public static int APP_ID = 39410710;
+    public static int APP_ID =39410710;
     public static String APP_HASH = "9b738f182693298243875fb53f3c7c27";
 
     // SafetyNet key for Google Identity SDK, set it to empty to disable
